eb91f7373f420a8384c3a658b854d8d87163ab64
domgarlor[user] <domgarlor@gmail.com>
2017-01-22 19:16:23 +0100

    S02C05 Declaración de constantes

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-let mensaje = "Hola amigos!!";
-if (1==1){
-  let mensaje = "Hasta la vista";
-  console.log(mensaje);
+const IMPUESTO_IVA = 21.05
+const USUARIO = {
+    nombre: "Domingo",
+    apellidos: "García Lorenzo"
 }
-console.log(mensaje);
+let mensaje = "Hola amigos!!";
+
+if (1 == 1) {
+    let mensaje = "Hasta la vista";
+    const IMPUESTO_IVA = 19.25;
+    USUARIO.nombre = "Francisco";
+    console.log('mensaje1:' + mensaje);
+    console.log('IVA1:' + IMPUESTO_IVA);
+    console.log('Usuario:' + USUARIO.nombre);
+}
+console.log('mensaje2:' +mensaje);
+console.log('IVA2:' + IMPUESTO_IVA);
+console.log('Usuario:' + USUARIO);
