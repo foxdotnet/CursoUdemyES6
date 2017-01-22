515112a65aeac5eb892cebb6baa6f082f140c9c6
domgarlor[user] <domgarlor@gmail.com>
2017-01-22 23:23:57 +0100

    Sección 3 Nuevos métodos con cadenas de caracteres - Strings

--- app.js
+++ app.js
@@ -1,18 +1,61 @@
-var funciones = [];
+let texto = "Hola Mundo"
+//Nuevas funciones de texto
+console.log( 'texto.startsWith("M", 5) ?' + texto.startsWith("M", 5) );
+console.log( 'texto.endsWith("la", 4) ?' + texto.endsWith("la", 4) );
+console.log('texto.includes("Mu", 8) ?' + texto.includes("Mu", 8));
 
-for (let i = 0; i <= 10; i++) {
-    funciones.push(function(i) {
-        console.log(i);
-    })
+//Repetición de strings
+let msg = "Bla ";
+console.log(msg.repeat(3));
+
+const ESPACIOS = 12;
+let nombres = ["Pilar", "Pedro", "Juan", "Antonia"];
+let telefonos = [123456789, 987654321, 555123456, 999485456];
+for  (i in nombres)
+{
+  console.log(nombres[i] + " ".repeat(ESPACIOS - nombres[i].length) + "|" + telefonos[i] + " - " + i);
 }
 
-funciones.forEach(function(fn)
+let USUARIO = {
+  nombre: "Domingo",
+  apellidos: "García Lorenzo",
+  eMail: "dogarlor@gamil.com",
+  edad: 47,
+}
+let multilinea = `Hola!!
+ Mi nombre es ${USUARIO.nombre} ${USUARIO.apellidos}.
+ Email de contacto: ${USUARIO.eMail}
+ Hora: ${Date()}`
+console.log(multilinea);
+
+//Cadenas literales con tags
+function etiquetar(literales, ...substituciones)
 {
-  fn();
-});
+  let resultado = "";
+  let i = 0;
+  for (i = 0; i < substituciones.length; i++)
+  {
+    resultado += literales[i];
+    resultado += substituciones[i];
+  }
+  resultado += literales[i];
 
-const USUARIO = {};
+  console.log(literales);
+  console.log(substituciones);
 
-USUARIO.nombre = "Domingo",
-USUARIO.apellidos = "García Lorenzo"
-console.log( USUARIO.nombre  );
+  return resultado;
+}
+
+let txtTags = etiquetar`Me llamo  ${USUARIO.nombre} ${USUARIO.apellidos} y tengo ${USUARIO.edad} años`
+console.log(txtTags);
+
+
+//Valores raw
+
+let txRaw = String.raw`\\inicio\\seguridad\\login`;
+console.log(txRaw);
+
+//let x = 10;
+//console.log(x.repeat(2));
+
+console.log("Hola" === `Hola`);
