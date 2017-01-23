95b49fc128890bbf22cf03d78d3f439d5851d2c1
domgarlor[user] <domgarlor@gmail.com>
2017-01-23 12:19:42 +0100

    Sección 5- Funciones de flecha. Arrow =>

--- app.js
+++ app.js
@@ -1,61 +1,94 @@
-let texto = "Hola Mundo"
-//Nuevas funciones de texto
-console.log( 'texto.startsWith("M", 5) ?' + texto.startsWith("M", 5) );
-console.log( 'texto.endsWith("la", 4) ?' + texto.endsWith("la", 4) );
-console.log('texto.includes("Mu", 8) ?' + texto.includes("Mu", 8));
-
-//Repetición de strings
-let msg = "Bla ";
-console.log(msg.repeat(3));
-
-const ESPACIOS = 12;
-let nombres = ["Pilar", "Pedro", "Juan", "Antonia"];
-let telefonos = [123456789, 987654321, 555123456, 999485456];
-for  (i in nombres)
-{
-  console.log(nombres[i] + " ".repeat(ESPACIOS - nombres[i].length) + "|" + telefonos[i] + " - " + i);
+////Funciones Arrow =>
+////Ejemplo de funciones
+var aleatorio1 = function(min, max) {
+    min = min || 1;
+    max = max || 100;
+    return Math.round(Math.random() * (max - min) + min);
 }
 
-let USUARIO = {
-  nombre: "Domingo",
-  apellidos: "García Lorenzo",
-  eMail: "dogarlor@gamil.com",
-  edad: 47,
-}
-let multilinea = `Hola!!
- Mi nombre es ${USUARIO.nombre} ${USUARIO.apellidos}.
- Email de contacto: ${USUARIO.eMail}
- Hora: ${Date()}`
-console.log(multilinea);
 
-//Cadenas literales con tags
-function etiquetar(literales, ...substituciones)
+
+let aleatorio2 = (min, max) => {
+    let res = Math.random() * (max - min) + min;
+    return Math.round(res)
+}
+
+console.log("aleatorio1: " + aleatorio1());
+console.log("aleatorio2: " + aleatorio2(1, 100));
+
+
+var obtenerLibro2 = function (p_id)
 {
-  let resultado = "";
-  let i = 0;
-  for (i = 0; i < substituciones.length; i++)
-  {
-    resultado += literales[i];
-    resultado += substituciones[i];
+  return {
+    id:p_id,
+    titulo:"El señor de las moscas",
+    autor: " William Golding",
   }
-  resultado += literales[i];
-
-  console.log(literales);
-  console.log(substituciones);
-
-  return resultado;
 }
 
-let txtTags = etiquetar`Me llamo  ${USUARIO.nombre} ${USUARIO.apellidos} y tengo ${USUARIO.edad} años`
-console.log(txtTags);
+var obtenerLibro1 = (p_id) => ({
+    id:p_id,
+    titulo:"El señor de las moscas",
+    autor: " William Golding",
+  });
 
 
-//Valores raw
+console.log(obtenerLibro1(1234));
 
-let txRaw = String.raw`\\inicio\\seguridad\\login`;
-console.log(txRaw);
+////Funciones anónimas
 
-//let x = 10;
-//console.log(x.repeat(2));
+var saludo1 = function (nombre){
+  return "Hola, " + nombre;
+}("Domingo");
 
-console.log("Hola" === `Hola`);
+console.log(saludo1);
+
+let saludo2 = (nombre =>`Hola, ${nombre}`)("Pilar");
+console.log(saludo2);
+
+////No hay cambios en el objeto this
+
+var manejador1 = {
+    id:"123",
+    init: function(){
+      document.addEventListener("click", (function(event){
+        console.log(this);
+        this.clickEnPagina(event.type);
+      }).bind(this), false );
+    },
+    clickEnPagina: function(tipo){
+      console.log("Manejando1" + tipo + " para el id: " + this.id);
+    }
+};
+manejador1.init();
+
+let manejador2 = {
+    id:"456",
+    init: function () {
+        document.addEventListener ( "click",
+        event => {
+          console.log(this);
+          this.clickEnPagina (event.type)
+        }, false );
+    },
+    clickEnPagina: function(tipo) { console.log( "Manejando2" + tipo + " para el id: " + this.id ) }
+  //clickEnPagina: tipo => console.log( "Manejando2" + tipo + " para el id: " + this.id )
+};
+manejador2.init();
+
+//Flechas y arreglos
+
+var arreglo = [4, 20 , 5, 22, 6, 3, 44];
+var arreglOrden = arreglo.sort(function(a,b){return a-b});
+console.log(arreglOrden);
+
+let arreglo2 = [6, 23 , 52, 212, 6, 3, 44];
+let arreglOrden2 = arreglo2.sort((a,b) => a-b);
+console.log(arreglOrden2);
+
+//Identificando funciones =>
+
+var restar = (a,b) => a - b;
+
+console.log(typeof(restar));
+console.log(restar instanceof Function);
